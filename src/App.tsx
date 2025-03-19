5dbb9a451601742c86a28e24e4a49c47761b1b6c
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-03-19 17:47:30 +0530

    adding hook intersection observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { useInfiniteScroll } from "./hooks/useInfiniteScroll";
+import { useRef } from "react"
+import { useIntersectionObserver } from "./hooks/useIntersectionObserver";
+
 const App = () => {
-  const {page,loading, showScrollToTop,ScrollToTop,reset} = useInfiniteScroll();
+  const nameRef = useRef<HTMLHeadingElement>(null);
+  const {isIntersect} = useIntersectionObserver({
+    targetRef:nameRef,
+    threshold:0.1,
+  });
+  console.log(isIntersect)
   return (
-    <div className=" w-full h-[150vh] flex flex-col items-center justify-center">
-      {loading && <div>Loading...</div>}
-      <h1>{page}</h1>
-      {showScrollToTop && (
-        <button onClick={()=>ScrollToTop()}>
-          Scroll to top
-        </button>
-      )}
-      <button onClick={reset}>
-        Reset
-      </button>
+    <div className=" w-full h-[200vh] flex flex-col items-center justify-center">
+      <div className=" w-full h-[100vh] bg-red-950 text-white flex items-center justify-center">
+        scroll
+      </div>
+      <div className="w-full h-[100vh] bg-slate-950 text-white flex items-center justify-center">
+        <h1 ref={nameRef}>Gaurav</h1>
+      </div>
     </div>
   )
 }
---
+++ src/hooks/useIntersectionObserver.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+interface Props<T>{
+    targetRef: React.RefObject<T | null>,
+    root?: T | null,
+    rootMargin?: string,
+    threshold?: number,
+}
+export const useIntersectionObserver = <T extends Element>({
+    targetRef,
+    root,
+    rootMargin,
+    threshold = 0.2,
+}: Props<T>) => {
+    const [isIntersect,setIsIntersect] = useState<boolean>(false);
+
+    useEffect(()=>{
+        const observer = new IntersectionObserver(([entry])=>{
+            console.log(entry);
+            if(entry.isIntersecting){
+                setIsIntersect(true);
+                return;
+            } else {
+                setIsIntersect(false);
+            }
+        },
+        {root, rootMargin, threshold});
+
+        const currentTarget = targetRef.current;
+        if(currentTarget){
+            observer.observe(currentTarget);
+        }
+
+        return ()=>{ 
+            if(currentTarget){
+                observer.unobserve(currentTarget);
+            }
+            observer.disconnect();
+        }
+    },[targetRef, rootMargin, root, threshold]);
+  return {isIntersect};
+}
