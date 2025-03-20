1b614abbe0f532c3b80a0e0f3d1b2cdf8ff9c113
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-03-20 19:10:23 +0530

    add useGetFileSize hook to format file sizes

---
+++ src/hooks/useGetFileSize.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+
+export const useGetFileSize = (size:number) => {
+ const [fileSize,setFileSize] = useState<string>("");
+
+ const fn = (s:number)=>{
+    if(s < 1024){
+        return `${s} B`
+    } else if(s < 1024 * 1024){
+        return `${(s/1024)} KB`
+    } else if(s < 1024 * 1024 * 1024){
+        return `${(s/1024 * 1024).toFixed(2)} MB`
+    } else {
+        return `${(s/1024*1024*1024).toFixed(2)} Gb`
+    }
+ }
+
+ useEffect(()=>{
+    setFileSize(fn(size))
+ },[size]);
+
+ return fileSize;
+}
