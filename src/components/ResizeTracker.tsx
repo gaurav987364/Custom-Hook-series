fe065ba5c8eee7d38c05e84dc51fd5f4cae22c03
gaurav987364 <sharmagourav9654@gmail.com>
2025-12-21 19:17:25 +0530

    c12

--- src/components/ResizeTracker.tsx
+++ src/components/ResizeTracker.tsx
@@ -24,7 +24,7 @@ const ResizeTracker = () => {
     const Callthis = useThrottle({fn:getCalls,delay:400,leading:true});
     console.log(Callthis("helloo"));
     console.log(Callthis("hi kes hoo"))
-    console.log(Callthis("me bahdiya hu tum btaaoa"))
+    console.log(Callthis("me bahdiya hu tum btaao"))
     console.log(Callthis("chal chutuyee khatam ho gya"))
     console.log(Callthis("okokoko chal bye...."))
   
