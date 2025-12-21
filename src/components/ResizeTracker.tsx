533bc5c5666e1ed1ed7e37593339221eff37794f
gaurav987364 <sharmagourav9654@gmail.com>
2025-12-21 19:15:41 +0530

    c4

--- src/components/ResizeTracker.tsx
+++ src/components/ResizeTracker.tsx
@@ -15,7 +15,7 @@ const ResizeTracker = () => {
         });
       },
       delay: 300,
-      trailing: true // Capture Fina
+      trailing: true // Capture Final
     });
 
     function getCalls(message: string){
